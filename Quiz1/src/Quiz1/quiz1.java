00ea1443edc83c33f6242a2b74978b1159aee1fa
Ukia <Ukia@LAPTOP-E8ELDQI6>
2017-02-19 21:37:01 -0500

    quiz1

---
+++ Quiz1/src/Quiz1/quiz1.java
@@ -0,0 +1,65 @@
+package Quiz1;
+
+import java.util.Scanner;
+
+public class quiz1 {
+
+	public static void QBrating() 
+
+	{
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("How many touchdowns?");
+
+		double touchdowns = input.nextDouble();
+
+		
+
+		System.out.println("How many total yards?");
+
+		double totalYards = input.nextDouble();
+
+		
+
+		System.out.println("How many interceptions?");
+
+		double interceptions = input.nextDouble();
+
+		
+
+		System.out.println("How many completions");
+
+		double completions = input.nextDouble();
+
+		
+
+		System.out.println("What is the total number of passes attempted?");
+
+		double passesAttempted = input.nextDouble();
+
+
+		double a = (completions/passesAttempted-0.3)*5;
+
+		double b = (totalYards/passesAttempted-0.3)*0.25;
+
+		double c = (touchdowns/passesAttempted)*20;
+
+		double d = 2.375-(interceptions/passesAttempted)*25;
+
+		double passerRating = 100*(a+b+c+d)/6;
+
+		System.out.println(passerRating);
+
+		input.close();
+
+	}
+
+
+	public static void main(String [] args) {
+
+		QBrating();
+
+	}
+
+}
